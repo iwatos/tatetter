870e9042c75b5e3dff544a740a0c852da3ec7a81
iwatos <nonbiriactive@gmail.com>
2019-03-22 09:08:12 +0900

    fix:リファクタリング　関数を細分化

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -27,86 +27,156 @@
     </div>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Content',
   data () {
     return {
       text: ""
     }
   },
   methods: {
-    getMatrixSize: function (matrix) {
-      const height = matrix.length
-      const width = matrix[0].length
-      const size = new Array(height, width)
-      matrix.forEach(element => {
-        if(element != width){
-          return new Array(-1, -1)
-        }         
-      });
-      return size
-    },
-
-    h2v: function(text) {
-      //英数字を全角にする
-      text = text.replace(/[!-~]/g, function(s) {return String.fromCharCode(s.charCodeAt(0) + 65248)})
-      text = text.replace(/ /g,"　")
-      text = text.replace(/ー/g,"｜")
-
-      //文字列の縦横の最大文字数を取得
-      const height = text.split("\n").length
-      var lengths = new Array(height)
-      text.split("\n").forEach(function(element, index){
+    
+    /**
+      * 文字列の高さと最大幅を算出します。
+      * @param {String} 幅と高さを知りたい文字列
+      * @return {Array} {高さ、幅}の配列 
+      */
+    getMatrixSize: function (str) {
+      let splitedStr = str.split("\n")
+      const height = splitedStr.length
+      let lengths = new Array(height)
+      splitedStr.forEach(function(element, index){
         lengths[index] = element.length
       });
       const width = Math.max(...lengths)
+      const size = new Array(height, width)
+      return size
+    },
 
-      //二次元配列で初期化
-      var horizonText = new Array(height);
-      for(let i = 0; i < height; i++) {
-        horizonText[i] = new Array(width).fill("　")
-      }
+    /**
+      * 文字列の半角文字を全角文字に変換する
+      * @param {String} 半角文字を含んだ文字列
+      * @return {String} 全角文字に変換された文字列
+      */
+     halfToFull: function(halfText) {
+       let fullText = halfText
+       fullText = fullText.replace(/[!-~]/g, function(s) {return String.fromCharCode(s.charCodeAt(0) + 65248)})
+       fullText = fullText.replace(/ /g,"　")
+       return fullText
+     },
 
-      //一字ずつ二次元配列に格納  
-      text.split("\n").forEach(function(e, i){
-        e.split("").forEach(function(f, j){
-          horizonText[i][j] = e[j]
+     
+    /**
+      * 文字列を一文字ずつの二次元配列に変換する
+      * @param {String} 文字列
+      * @return {Array} 二次元配列
+      */
+     stringToMatrix: function(str) {
+       let size = this.getMatrixSize(str)
+       const height = size[0]
+       const width = size[1]
+
+       //二次元配列を作成し初期化
+       let matrix = new Array(height);
+       for(let i = 0; i < height; i++) {
+        matrix[i] = new Array(width).fill("　")
+       }
+       
+       //一字ずつ二次元配列に格納  
+       str.split("\n").forEach(function(e, i){
+         e.split("").forEach(function(f, j){
+           matrix[i][j] = e[j]
         })
-      })
+       })
+       
+       return matrix
+     },
 
-      var verticalText = new Array(width);
-      for(let i = 0; i < width; i++) {
-        verticalText[i] = new Array(height).fill("　")
+    /**
+      * 長方形の二次元配列を時計回り90度回転させる
+      * @param {Array} 回転前の二次元配列
+      * @return {Array} 回転後の二次元配列
+      */
+     rotateMatrix: function(matrix) {
+      //高さ、幅の取得
+      const height = matrix.length
+      const width = matrix[0].length
+
+      //幅が0、すなわち空文字の時は終了
+      if(width <= 0){
+        return matrix
       }
 
+      //二次元配列の初期化
+      let rotatedMatrix = new Array(width);
+      for(let i = 0; i < width; i++) {
+        rotatedMatrix[i] = new Array(height).fill("　")
+      }
+
+      //回転させて値を挿入
       for (let i = 0; i < height; i++) {
             for (let j = 0; j < width; j++) {                
-                verticalText[j][i] = horizonText[height-i-1][j];
+                rotatedMatrix[j][i] = matrix[height-i-1][j];
             }
         }
+      return rotatedMatrix
+     },
 
-      verticalText.forEach(function (e,i,v) {
+    /**
+      * 二次元配列を文字列に変換する
+      * @param {Array} 二次元配列
+      * @return {String} 文字列
+      */
+     matrixToString: function(matrix) {
+      matrix.forEach(function (e,i,v) {
         v[i] = e.join("")
       })
-      verticalText = verticalText.join("\n")
+      let str = matrix.join("\n")
+      return str
+     },
 
+    /**
+      * 文字列を縦書きに変換する
+      * @param {String} 文字列
+      * @return {String} 文字列
+      */
+    horizonToVertical: function(text) {
+      let fullText = this.halfToFull(text).replace(/ー/g,"｜")//半角を全角にし"ー"を"|"に置換する
+      let horizonMatrix = this.stringToMatrix(fullText)//文字列を二次元配列に変換
+      let verticalMatrix = this.rotateMatrix(horizonMatrix)//二次元配列を時計回り90度回転
+      let verticalText = this.matrixToString(verticalMatrix)
       return verticalText;
     },
 
-    blankToDot: function(text){
-      let t = text.slice(0, 1)
-      if(t == "　"){
-        return "．" + text.slice(1) 
+    /**
+      * 文字列先頭の空白を"．"に変換する
+      * @param {String} 文字列
+      * @return {String} 文字列
+      */
+    blankToDot: function(str){
+      let s = str.slice(0, 1)
+      if(s == "　"){
+        return "．" + str.slice(1) 
       }
-      return text
+      return str
     }
   },
   computed: {
+    /**
+      * 縦書き文字列を算出する
+      * @return {String} 縦書き文字列
+      */
     verticalText: function () {
-      let text = this.h2v(this.text)
-      return this.blankToDot(text)
+      let text = this.horizonToVertical(this.text)
+      text = this.blankToDot(text)
+      return text
     },
+    /**
+      * ツイートページ遷移用のURLを算出する
+      * @return {String} 縦書き文字列
+      */
     hreftext: function () {
       let text = "text=" + this.verticalText.replace(/\n/g, "%0A") +"%0A%0A"
       let url = "url=" + "https://tatetter.netlify.com"
